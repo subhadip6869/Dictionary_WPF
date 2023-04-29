bf58ceab9580416ddab1da55bdcbad7153d88c6a
Subhadip Dutta <subhadipdutta3000@gmail.com>
2023-04-29 20:17:52 +0530

    fixed lag issue for combobox

--- Dictionary/MainWindow.xaml.cs
+++ Dictionary/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Threading;
@@ -36,7 +37,7 @@ namespace Dictionary
             try
             {
                 if (conn.State != ConnectionState.Open) conn.Open();
-                query = "SELECT word FROM BengaliDictionary ORDER BY word";
+                query = "SELECT word FROM BengaliDictionary_36 ORDER BY word";
                 cmd = new OleDbCommand(query, conn);
                 OleDbDataReader dr = cmd.ExecuteReader();
                 while (dr.Read())
@@ -71,13 +72,20 @@ namespace Dictionary
             }
         }
 
+        private void search_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = search_box.SelectedIndex;
+            data_listbox.SelectedIndex = (int)index;
+            data_listbox.ScrollIntoView(data_listbox.Items[index]);
+        }
+
         private void data_listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
             {
                 //..........< Read Meaning >..........
                 if (conn.State != ConnectionState.Open) conn.Open();
-                query = "SELECT id, meaning FROM BengaliDictionary WHERE word = '" + data_listbox.SelectedItem.ToString().Replace("'", "''") + "'";
+                query = "SELECT id, meaning FROM BengaliDictionary_36 WHERE word = '" + data_listbox.SelectedItem.ToString().Replace("'", "''") + "'";
                 cmd = new OleDbCommand(query, conn);
                 OleDbDataReader fetchdata = cmd.ExecuteReader();
                 while (fetchdata.Read())
