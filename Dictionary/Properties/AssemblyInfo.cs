33e2c535ebc1eb031b5a8b907ad84911603286eb
Subhadip Dutta <subhadipdutta3000@gmail.com>
2023-07-20 09:50:29 +0530

    added developer name, configured for default publish by vs

--- Dictionary/Properties/AssemblyInfo.cs
+++ Dictionary/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Dictionary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SD Softwares")]
 [assembly: AssemblyProduct("Dictionary")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.1.2")]
+[assembly: AssemblyFileVersion("1.2.1.2")]
+[assembly: NeutralResourcesLanguage("en")]
